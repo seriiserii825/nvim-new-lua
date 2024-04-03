f188cbadcef1908c30eb0e7508ed1a8cf193d8d5
serii <seriiburduja@gmail.com>
2024-04-03 10:15:09 +0300

    feat: nvim-cmp capabilities

--- lua/serii/plugins/nvim-cmp.lua
+++ lua/serii/plugins/nvim-cmp.lua
@@ -62,8 +62,8 @@
   })
 
   -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    capabilities = capabilities
-  }
+  -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  --   capabilities = capabilities
+  -- }
