d80eaf3d988d47271e4e8610069b41aad2c3b316
serii <seriiburduja@gmail.com>
2024-04-03 18:56:51 +0300

    feat: snippets rename folder

--- lua/serii/plugins/ultisnipets.lua
+++ lua/serii/plugins/ultisnipets.lua
@@ -4,5 +4,7 @@ vim.g.UltiSnipsJumpBackwardTrigger="<c-z>"
 
 -- " If you want .UltiSnipsEdit to split your window.
 vim.g.UltiSnipsEditSplit="vertical"
--- vim.g.UltiSnipsSnippetsDir = "/home/serii/.config/nvim/UltiSnips"
-vim.g.UltiSnipsSnippetsDir = "~/.vim/UltiSnips"
+vim.g.UltiSnipsSnippetsDir = "/home/serii/.config/nvim/UltiSnips"
+
+-- vim.g.UltiSnipsSnippetsDir = "~/Documents/Apps/nvim-new-lua/UltiSnips"
+-- vim.g.UltiSnipsSnippetDirectories = {"/home/serii/Documents/Apps/nvim-new-lua/UltiSnips", "UltiSnips"}
